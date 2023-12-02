e0d8244497a1c595c9e885e345827bceca50969d
TaRa1999R <rezaietara78@gmail.com>
2023-12-02 10:38:08 +0330

    assignment27

--- 1.py
+++ 1.py
@@ -5,9 +5,9 @@ import numpy
 img = cv2.imread ("1_input.jpg")
 img = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
 
-threshold = 100
-img [img > threshold] = 255
-img [img <= threshold] = 0
+# threshold = 100
+# img [img > threshold] = 255
+# img [img <= threshold] = 0
 
 cv2.imshow ("result 1" , img)
 cv2.waitKey ()
