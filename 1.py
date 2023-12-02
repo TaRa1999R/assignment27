20450bc5fa4fdeb5e922e6584ea08f67a37205da
TaRa1999R <rezaietara78@gmail.com>
2023-12-02 14:15:19 +0330

    assignment27

--- 1.py
+++ 1.py
@@ -5,9 +5,11 @@ import numpy
 img = cv2.imread ("1_input.jpg")
 img = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
 
-# threshold = 100
-# img [img > threshold] = 255
-# img [img <= threshold] = 0
+print(img.shape)
+threshold = 140
+_, img_batman = cv2.threshold (img , threshold , 255 , cv2.THRESH_BINARY_INV)
+cv2.putText (img_batman , "BATMAN" , (375 , 510) , cv2.FONT_ITALIC , 2 , (255 , 255 , 255))
 
-cv2.imshow ("result 1" , img)
+cv2.imshow ("result 1" , img_batman)
 cv2.waitKey ()
+cv2.imwrite ("1_output.jpg" , img_batman)
