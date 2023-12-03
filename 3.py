a0f9c2c0d485f2e7500842b82d0cb99c836c385e
TaRa1999R <rezaietara78@gmail.com>
2023-12-03 11:45:36 +0330

    assignment27

--- 3.py
+++ 3.py
@@ -9,16 +9,20 @@ writer = cv2.VideoWriter ("3_output.mp4" , cv2.VideoWriter_fourcc(*'mpv4') , 10 
 snow_place = []
 
 while True :
-    snow_img = img
+    img = cv2.imread ("3_input.jpg")
+    img = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
     new_snow = random.randint (0 , cols)
     snow_place.append ({"row" : 0 , "col" : new_snow})
     for snow in snow_place :
-        cv2.circle (snow_img , (snow["col"] , snow["row"]) , 3 , (255 , 255 , 255) , -1)
+        cv2.circle (img , (snow["col"] , snow["row"]) , 3 , (255 , 255 , 255) , -1)
         snow["row"] += 8
 
         if snow["row"] >= rows :
             snow_place.remove (snow)
 
     cv2.imshow ("result 3" , img)
-    if cv2.waitKey (100) & 0xFF == ord ('q') :
+    writer.write (img)
+    if cv2.waitKey (40) & 0xFF == ord ('q') :
         break
+
+writer.release ()
