33531f4edefc77c4445905a29fa3fa638f8016bb
TaRa1999R <rezaietara78@gmail.com>
2023-12-03 14:08:25 +0330

    assignment27

--- 4.py
+++ 4.py
@@ -13,9 +13,25 @@ while True :
     blur_fram = cv2.blur (fram, (30 , 30))
     blur_fram[179:302 , 259:382] = check_color
 
+    # for pix in check_color :
+        # print(pix)
+        # if pix.all () >= 240 :
+            # print ("White")
+            # cv2.putText (blur_fram , "WHITE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (0 , 0 , 0))
+
+        # elif pix.all () <= 5 :
+            # print ("Black")
+            # cv2.putText (blur_fram , "BLACK" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (255 , 255 , 255))
+
+
+        # else :
+            # print ("Gray")
+            # cv2.putText (blur_fram , "GRAY" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (150 , 150 , 150))
+
+
     cv2.imshow ("result 4" , blur_fram)
     # writer.write (img)
-    if cv2.waitKey (25) & 0xFF == ord ('q') :
+    if cv2.waitKey (100) & 0xFF == ord ('q') :
         break
 # cap.release()
 # writer.release ()
