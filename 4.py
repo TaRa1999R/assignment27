c2c62d545b63f168dd132868c6f5aad3e0d04c4d
TaRa1999R <rezaietara78@gmail.com>
2023-12-03 14:39:00 +0330

    assignment27

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@
 import cv2
 
 cap = cv2.VideoCapture (0)
-# writer = cv2.VideoWriter ("4_output.mp4" , cv2.VideoWriter_fourcc(*'mpv4') , 10 , (480 , 640))
+writer = cv2.VideoWriter ("4_output.mp4" , cv2.VideoWriter_fourcc(*'mpv4') , 10 , (480 , 640))
 
 while True :
     _ , fram = cap.read ()
@@ -12,26 +12,22 @@ while True :
     check_color = fram[179:302 , 259:382]
     blur_fram = cv2.blur (fram, (30 , 30))
     blur_fram[179:302 , 259:382] = check_color
+    
+    for row in range (205 , 265) :
+        for col in range (290 , 350) :
+            if blur_fram[row , col] >=250 :
+                cv2.putText (blur_fram , "WHITE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (0 , 0 , 0))
 
-    # for pix in check_color :
-        # print(pix)
-        # if pix.all () >= 240 :
-            # print ("White")
-            # cv2.putText (blur_fram , "WHITE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (0 , 0 , 0))
+            elif blur_fram[row , col] <= 10 :
+                cv2.putText (blur_fram , "BLACK" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (255 , 255 , 255))
 
-        # elif pix.all () <= 5 :
-            # print ("Black")
-            # cv2.putText (blur_fram , "BLACK" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (255 , 255 , 255))
-
-
-        # else :
-            # print ("Gray")
-            # cv2.putText (blur_fram , "GRAY" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (150 , 150 , 150))
+            else :
+                cv2.putText (blur_fram , "GRAY" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (0 , 0 , 0))
 
 
     cv2.imshow ("result 4" , blur_fram)
-    # writer.write (img)
+    writer.write (blur_fram)
     if cv2.waitKey (100) & 0xFF == ord ('q') :
         break
-# cap.release()
-# writer.release ()
+
+writer.release ()
